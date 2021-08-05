4663d15c2e52506991cf646c7d68b9333e81831c
firebrand88 <46079650+firebrand88@users.noreply.github.com>
2021-08-04 19:49:28 -0500

    Uploading initial PyPoll code and results
    Results verified, README file updated

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,89 @@
 import csv
-with open('names_and_emails.csv', 'r') as csv_file:
+import os
+import pandas as pd
+
+election_csv = os.path.join("..","Resources","election_data.csv")
+
+PyVoter = []
+Candidates = []
+
+df = pd.read_csv(election_csv)
+
+def main(PyVoter,Candidates,kvotes,cvotes,lvotes,ovotes):
+    with open('PyVote_results.txt','w') as new_file:
+        totalvotes = len(df)
+        candlist = df["Candidate"].unique()  #get list of candidates in dataset
+        # print(len(candlist))  # ['Khan' 'Correy' 'Li' "O'Tooley"]
+        # nullvalues = df.isnull().sum()  #check to see if there are blank cells
+        # print(nullvalues)  
+
+        pctk = kvotes/totalvotes
+        pctc = cvotes/totalvotes
+        pctl = lvotes/totalvotes
+        pcto = ovotes/totalvotes
+        candies = {
+            "Khan": pctk,
+            "Correy": pctc,
+            "Li": pctl,
+            "O'Tooley": pcto
+        }
+
+        pctwin = 0
+
+        for i in candies:
+            if candies[i] > pctwin:
+                pctwin = candies[i]
+
+        def get_key(x):
+            for k, v in candies.items():
+                if x == v:
+                    return k
+
+        wincd = get_key(pctwin)
+
+        print("Election Results")
+        new_file.write("Election Results\n")
+        print("------------------------")
+        new_file.write("------------------------\n")
+        print(f"Total Votes: {totalvotes:,}")
+        new_file.write(f"Total Votes: {totalvotes:,}\n")
+        print("------------------------")
+        new_file.write("------------------------\n")
+        print(f"Khan: {pctk:.1%} ({kvotes:,})")
+        new_file.write(f'Khan: {pctk:.1%} ({kvotes:,})\n')  #Khan data
+        print(f"Correy: {pctc:.1%} ({cvotes:,})")
+        new_file.write(f'Correy: {pctc:.1%} ({cvotes:,})\n')  #Correy data
+        print(f"Li: {pctl:.1%} ({lvotes:,})")
+        new_file.write(f'Li: {pctl:.1%} ({lvotes:,})\n')  #Li data
+        print(f"O'Tooley: {pcto:.1%} ({ovotes:,})")
+        new_file.write(f"O'Tooley: {pcto:.1%} ({ovotes:,})\n")  #O'Tooley data
+        print("------------------------")
+        new_file.write("------------------------\n")
+        print(f"Winner: {wincd}")
+        new_file.write(f"Winner: {wincd}\n")
+        print("------------------------")
+        new_file.write("------------------------\n")
+
+
+with open(election_csv, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
-    with open('new_names.csv', 'w') as new_file:
-        csv_writer = csv.writer(new_file, delimiter='\t')
-        for line in csv_reader:
-            csv_writer.writerow(line)
+
+    next(csv_reader)
+
+    kvotes = 0
+    cvotes = 0
+    lvotes = 0
+    ovotes = 0
+
+    for row in csv_reader:
+        if row[2] == 'Khan':
+            kvotes = kvotes + 1
+        elif row[2] == 'Correy':
+            cvotes = cvotes + 1
+        elif row[2] == 'Li':
+            lvotes = lvotes + 1
+        else:
+            ovotes = ovotes + 1
+
+
+main(PyVoter,Candidates,kvotes,cvotes,lvotes,ovotes)
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Python Challenge
 
 This project is meant to exercise how to:
+
 *Read .csv files
 *Extract data from .csv files
 *Run calculations on extracted data
+*Use dictionaries and key value lookups
 *Display results both in the terminal and a .txt output file
 
 
@@ -13,3 +15,9 @@ In this project, we read the budget_data.csv file in our Resources folder.  We e
 
 The results are printed both in the Python terminal and in an output text file for easy reading.
 
+
+## PyPoll
+
+In this project, we read through election_data.csv file in our Resources folder.  We extract the data to calculate the total number of votes cast, how many votes each candidate received both in count and percent of total, and determine the winner based on the percent popular vote calculation.
+
+The results are printed both in the Python terminal and in an output text file for easy reading.
